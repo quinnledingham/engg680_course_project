04a3068ecb3af7ba126e917465483dbef6bca4f1
quinnledingham <quinnledingham@gmail.com>
2024-10-06 23:32:16 -0600

    caching data locally

--- main.py
+++ main.py
@@ -23,17 +23,17 @@ class Vector2:
 api_host = 'https://climate.weather.gc.ca'
     
 class Stations:
-    database = pd.read_csv('Station Inventory EN.csv')
+    df = pd.read_csv('Station Inventory EN.csv')
 
     @classmethod
     def get_coordinates(self, row):
-        latitude = self.database.iloc[row]['Latitude (Decimal Degrees)']
-        longitude = self.database.iloc[row]['Longitude (Decimal Degrees)']
+        latitude = self.df.iloc[row]['Latitude (Decimal Degrees)']
+        longitude = self.df.iloc[row]['Longitude (Decimal Degrees)']
         return Vector2(latitude, longitude)
 
     @classmethod
     def print(self):
-        print(self.database)
+        print(self.df)
 
     # used to find stations close to low air quality
     # find the num stations closest to position
@@ -44,72 +44,80 @@ class Stations:
 
     @classmethod
     def get_station(self, row):
-        return self.database.iloc[row]
+        return self.df.iloc[row]
 
     @classmethod
     def get_station_with_name(self, name):
-        ids = self.database.index[self.database['Name'] == name].tolist()
+        ids = self.df.index[self.df['Name'] == name].tolist()
         return Stations.get_station(ids[0])
 
 def get_data(station, day, month, year):
-    # this link returns the whole month regardless of day specified
-    url = f"https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv"\
-            f"&stationID={station['Station ID']}"\
-            f"&Year={year}"\
-            f"&Month={month}"\
-            f"&Day=1"\
-            f"&timeframe=1"    
-    
-    response = requests.get(url)
+    local_path = f"./data_cache/climate_data/{station['Station ID']}_{year}_{month}.csv"
 
-    data = BytesIO(response.content)
-    df = pd.read_csv(data)
+    if not os.path.exists(local_path):
+        # this link returns the whole month regardless of day specified
+        url = f"https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv"\
+              f"&stationID={station['Station ID']}"\
+              f"&Year={year}"\
+              f"&Month={month}"\
+              f"&Day=1"\
+              f"&timeframe=1"    
+    
+        response = requests.get(url)
+
+        data = BytesIO(response.content)
+        df = pd.read_csv(data)
+        df.to_csv(local_path)
+    else:
+        df = pd.read_csv(local_path)
 
     return df
 
-def extract_zip(zip):
-    print(type(zip.namelist()))
-    # return { name: zip.read(name) for name in zip.namelist() }
-
-def naps_data(year):
-    local_path = f"./data/naps/PM25_{year}.zip"
+def naps_data(day=0, month=0, year=0):
+    local_path = f"./data_cache/naps/{year}.csv"
+    
     # check local store
-    if os.path.exists(local_path):
-        data = open(local_path, "rb")
-    else:
+    if not os.path.exists(local_path):
+        # data comes in zip by year
         url = f"https://data-donnees.az.ec.gc.ca/api/file?path=%2Fair%2Fmonitor%2Fnational-air-pollution-surveillance-naps-program%2FData-Donnees%2F{year}%2FIntegratedData-DonneesPonctuelles%2F{year}_NAPSReferenceMethodPM25-PM25MethodeReferenceSNPA.zip"
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
         }
         response = requests.get(url, headers=headers)
         assert response.status_code == 200
-        data = BytesIO(response.content)
 
-        f = open(local_path, "x")
-        f.write(response.content)
-        f.close()
-    
-        zip = ZipFile(data)
-        
-        files = zip.namelist()
-        out = []
-        for name in files:
-            check = '_FR' in name or name.endswith(".xlsx")
+        # write zip to csv in data cache
+        res_data = BytesIO(response.content)
+        in_zip = ZipFile(res_data)
+
+        files = []
+        for filename in in_zip.namelist():
+            check = '_FR' in filename or filename.endswith(".xlsx")
             if not check:
-                out.append(name)
-        files = out
+                file = in_zip.read(filename)
+                files.append(BytesIO(file))
+        in_zip.close()
+        df = pd.concat(map(pd.read_csv, files), ignore_index=True)
+        df.to_csv(local_path)
+        
+    df = pd.read_csv(local_path)
+    return df
+
+def create_data_cache():
+    paths = ["./data_cache", "./data_cache/naps", "./data_cache/climate_data/"]
+    for path in paths:
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+def main():    
+    create_data_cache()
     
-    #file = zip.read(files[0])
-    #df = pd.read_csv(BytesIO(file))
-    #print(df.head())
-    
-def main():
     station = Stations.get_station_with_name("FOGGY LO")
     print(f'Station ID: {station['Station ID']}')
     print(f'Climate ID: {station['Climate ID']}')
-#    data = get_data(station, 24, 9, 2024)
-#    print(data['Wind Spd (km/h)'])
-    naps_data(2023)
+    data = get_data(station, 24, 9, 2024)
+    # print(data['Wind Spd (km/h)'])
+    print(naps_data(year=2021))
     
 if __name__ == '__main__':
     main()
